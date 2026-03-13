264212749de4ed3df1599e5fd5c2521659e6a6ab
Kamronbek <118995322+A-Kamronbek@users.noreply.github.com>
2026-03-13 17:00:33 +0000

    .

--- session-1/task2/matrix_vector.c
+++ session-1/task2/matrix_vector.c
@@ -9,6 +9,7 @@ int main( void ) {
    float a[4][4];
    float b[4];
    float c[4];
+   float d;
 
    /*
    Intialise the matrix a and vector b entries to 1. 
@@ -23,7 +24,14 @@ int main( void ) {
       b[k] = 1;
    }
 
-   
+   for (int k=0; k<4; ++k){
+      d = 0;
+      for (int j=0; j<4; ++j){
+         d = d + (a[k][j]*b[j]);
+      }
+      c[k] = d;
+      printf("%f\n", c[k]);
+   }
    
    return 0;
 }
--- session-1/task2/transpose.c
+++ session-1/task2/transpose.c
@@ -3,16 +3,40 @@
  * Transpose of a matrix
  */
 
- #include <stdio.h>
+#include <stdio.h>
 
- int main( void ) {
-    int a[4][4];
+int main( void ) {
+   int a[4][4];
+   int b;
 
-    /*
-    Intialise the matrix a to 2i-j.
-    Print out matrix a. 
-    Write code for the transpose - you can use other variables as necessary but not a cpoy of the matrix 
-    Print the transpose.
-    */
-    return 0;
- }
+   /*
+   Intialise the matrix a to 2i-j.
+   Print out matrix a. 
+   Write code for the transpose - you can use other variables as necessary but not a cpoy of the matrix 
+   Print the transpose.
+   */
+
+   for (int i=0; i<4; ++i){
+      for (int j=0; j<4; ++j){
+         a[i][j] = 2*i-j;
+         printf("%d ", a[i][j]);
+
+      }
+      printf("\n");
+   }
+
+   printf("=========================\n");
+   for (int i=0; i<4; ++i){
+      for (int j=i; j<4; ++j){
+         b = a[i][j];
+         a[i][j] = a[j][i];
+         a[j][i] = b;
+         
+      }
+      for (int j=0; j<4; ++j){
+         printf("%d ", a[i][j]);
+      }
+      printf("\n");
+   }
+   return 0;
+}
