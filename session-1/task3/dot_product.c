6b19ea8bc39dc96584d2b5e8ca939740e1ad538f
Kamronbek <118995322+A-Kamronbek@users.noreply.github.com>
2026-03-13 18:01:45 +0000

    .

--- session-1/task3/dot_product.c
+++ session-1/task3/dot_product.c
@@ -1,32 +1,42 @@
 
 /*
  * Dot product of 2 vectors
- */
+*/
 
- #include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
- int main( void ) {
-    int n = 5;
-    float *a, *b, *d;  // stack-based pointers
+int main( void ) {
+   int n = 5;
+   float *a, *b, *d;  // stack-based pointers
+   float sum=0;
 
-    // allocation of heap memory for vectors
-    a = calloc( n, sizeof(float) );
-    b = calloc( n, sizeof(float) );
-    d = calloc( n, sizeof(float) );
+   // allocation of heap memory for vectors
+   a = calloc( n, sizeof(float) );
+   b = calloc( n, sizeof(float) );
+   d = calloc( n, sizeof(float) );
 
-    /*
-    Code to initialise the vectors with numerical data 
-    Code to compute the dot product 
-    - does this differ from the initial code?
-    - note that loop now run k=0;k<n 
-    Store your answer in d
-    print your final answer
-    */
+   /*
+   Code to initialise the vectors with numerical data 
+   Code to compute the dot product 
+   - does this differ from the initial code?
+   - note that loop now run k=0;k<n 
+   Store your answer in d
+   print your final answer
+   */
+   for (int k=0; k<n; ++k){
+      a[k] = 2-k;
+      b[k] = k;
+   }
+   for (int k=0; k<n; ++k){
+      sum = sum + (a[k]*b[k]);
+   }
+   printf("%f\n", sum);
 
-    // explicit deallocation of heap memory before exit */
-    free(a);
-    free(b);
-    free(d);
+   // explicit deallocation of heap memory before exit */
+   free(a);
+   free(b);
+   free(d);
 
-    return 0;
- }
+   return 0;
+}
--- session-1/task3/matrix_vector.c
+++ session-1/task3/matrix_vector.c
@@ -4,11 +4,41 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main( void ) {
     int n = 4;           
     float **a, *b, *c;
+    int d;
 
+    a = calloc(n, sizeof(float *));
+    b = calloc(n, sizeof(float));
+    c = calloc(n, sizeof(float));
+
+    for (int i=0; i<n; ++i){
+        a[i] = calloc(n, sizeof(float));
+    }
+
+    for (int k=0; k<n; ++k){
+        for (int j=0; j<n; ++j){
+            a[k][j] = 1;
+        }
+        b[k] = 1;
+    }
+
+    for (int k=0; k<n; ++k){
+        d = 0;
+        for (int j=0; j<n; ++j){
+            d = d + (a[k][j]*b[j]);
+        }
+        c[k] = d;
+        printf("%f\n", c[k]);
+
+    }
+
+    free(a);
+    free(b);
+    free(c);
     /*
     Dynamically allocate the matrix a and vectors b and c using size n
     Intialise the matrix a and vector b entries to 1. 
